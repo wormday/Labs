d07a34b3a3ff11df1aabe29805374b235221b1a4
sun wu qiang <sunwuqiang@gmail.com>
2013-07-07 00:52:19 +0800

    string

---
+++ Ruby/006.string.rb
@@ -0,0 +1,12 @@
+"1234abcd".length;#×Ö·û´®³¤¶È
+"123456".include? "2";
+"123456".include? "4567";
+"123".insert(2,'X')
+"123".insert(-1,'X')
+"123".insert(-4,'X')
+"1 2 34 567".split
+"1 2 34 567".split("5")
+3.times{|x| puts x}
+3.times{puts '4444'}
+puts "123456".gsub('4','*')
+puts '333r'.replace("d")
