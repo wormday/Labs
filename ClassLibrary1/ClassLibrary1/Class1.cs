8c3ab6862dcda3ddba003dec9d9e33a3b720377e
sun wu qiang <sunwuqiang@gmail.com>
2014-07-13 23:26:30 +0800

    js

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public void a()
+        {
+            var t = new Moq.Mock<IA>();
+            t.Setup(o => o.Add("1", "2", "3"));
+            t.Object.Add("1", "2", "3");
+        }
+    }
+    public interface IA
+    {
+        int Add(string a, string b, string c);
+    }
+}
---
+++ ClassLibrary1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var mock = new Moq.Mock<IAccess>();
+            mock.Setup(o => o.Add(Moq.It.IsAny<string>(), Moq.It.IsAny<string>())).Returns(100);
+            mock.Setup(o => o.Get(3)).Verifiable();
+            mock.Setup(o => o.Get(7)).Verifiable();
+            var b = new Biz(mock.Object);
+            var rtn=b.AddNewUser("1", "2", "3");
+            
+            Assert.AreEqual(rtn, 100);
+            mock.Verify();
+
+
+
+        }
+
+    }
+
+    public class Biz
+    {
+        IAccess acc = null;
+
+        public Biz(IAccess acc)
+        {
+            this.acc = acc;
+        }
+
+        public int AddNewUser(string a, string b, string c)
+        {
+            acc.Get(7);
+            //acc.Get(3);
+            return acc.Add(a, b + c);
+        }
+    }
+
+    public interface IAccess
+    {
+         int Add(string a, string b);
+         string Get(int id);
+    }
+}
